f4f98ebd54fd916dc254251e1f6f7b86f5918c00
negociosHFmorales <negocios.HFmorales@gmail.com>
2025-08-28 20:26:17 -0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Tus claves secretas
-FOOTBALL_API_KEY = os.getenv('FOOTBALL_API_KEY')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT = os.getenv('TELEGRAM_CHAT')
-
-# Configuración básica
-CONFIANZA_MINIMA = 0.60  # Solo apostar si hay 60% de confianza
